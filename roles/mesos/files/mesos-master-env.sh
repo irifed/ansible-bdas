68eb0044b442d5014da8b5d5c5c38dd3f6e8cc5f
IRINA FEDULOVA <i.fedulova@ru.ibm.com>
2014-09-10 16:34:05 +0400

    mesos

---
+++ roles/mesos/files/mesos-master-env.sh
@@ -0,0 +1,9 @@
+# This file contains environment variables that are passed to mesos-master.
+# To get a description of all options run mesos-master --help; any option
+# supported as a command-line option is also supported as an environment
+# variable.
+
+# Some options you're likely to want to set:
+export MESOS_log_dir=/var/log/mesos
+export MESOS_work_dir=/var/run/mesos
+export MESOS_isolation=cgroups
