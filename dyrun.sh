4fc9589d59a49896cd5005307cfbfe4cc674e751
kbroughton <kbroughton@c12e.com>
2015-02-10 13:29:48 -0600

    move vars to defaults, remove include_vars and vars_files

--- dyrun.sh
+++ dyrun.sh
@@ -4,5 +4,5 @@ KEY_FILE=$1
 shift
 EXTRA_ARGS="$@"
 cmd="ansible-playbook  --private-key=$KEY_FILE --user=ubuntu  -e ansible_ssh_user=ubuntu -vvvv site.yml ${EXTRA_ARGS}" 
-echo "$cmd"
+echo "${cmd}"
 $cmd
